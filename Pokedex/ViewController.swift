a12e67a8892f6f442b7fea4406e12d86b2fee98e
Alex-Besson <alex_j67@hotmail.com>
2015-11-02 01:39:39 -0500

    Setup search bar and functionality. Music is now off by default.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -9,15 +9,23 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
-//    @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
     
     var musicPlayer : AVAudioPlayer!
+    
+    var inSearchMode = false
+    var filteredPokemon = [Pokemon]()
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var btnMusic: UIButton!
+    
     @IBAction func musicButtonPressed(sender: UIButton!) {
+        
         if musicPlayer.playing {
             musicPlayer.stop()
             sender.alpha = 0.2
@@ -25,10 +33,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             musicPlayer.play()
             sender.alpha = 1
         }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
         
         initAudio()
         parsePokemonCSV()
@@ -41,11 +52,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
             musicPlayer.prepareToPlay()
             musicPlayer.numberOfLoops = -1
-            musicPlayer.play()
+            musicPlayer.stop()
         } catch let err as NSError {
             print(err.debugDescription)
             
         }
+        
+        btnMusic.alpha = 0.2
     }
     
     func parsePokemonCSV() {
@@ -71,14 +84,24 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode {
+            return filteredPokemon.count
+        } else {
+            return pokemon.count
+        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokemonCell {
             
-            let poke = pokemon[indexPath.row]
+            let poke : Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemon[indexPath.row]
+            } else {
+                poke = pokemon[indexPath.row]
+            }
             
             cell.configureCell(poke)
             
@@ -95,6 +118,19 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            view.endEditing(true)
+            collection.reloadData()
+        } else {
+            inSearchMode = true
+            let lower = searchBar.text!.lowercaseString
+            filteredPokemon = pokemon.filter({$0.name.rangeOfString(lower) != nil})
+            collection.reloadData()
+        }
     }
 
 
